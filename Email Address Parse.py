d5183c0d679105814612ffebc94fb663d3a5041b
AdamOlTR <95968691+AdamOlTR@users.noreply.github.com>
2022-03-16 14:32:35 +0100

    Add files via upload

---
+++ Email Address Parse.py
@@ -0,0 +1,4 @@
+mail = input("Enter the email (Gmail):") #Only works with Gmail
+print("The email is: " + mail)
+Username = mail[:-10] # #get all but the 10 last character
+print("The name is: " + Username)
